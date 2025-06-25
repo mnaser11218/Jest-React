61e7b98e9b91638afde7e4b45236262b8dd137a8
Mohammed Naser <mnaser11218@gmail.com>
2025-06-24 22:17:56 -0400

    added todo component

---
+++ my-app/src/components/todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = ({}) => {
+  return (
+    <div>
+    Hello, this is a todo item!
+    </div>
+  );
+};
+
+export default Todo;
+// This component represents a single todo item.
