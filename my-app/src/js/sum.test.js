8216017cc1bac572a047d1e6171cb34b43f2205d
Mohammed Naser <mnaser11218@gmail.com>
2025-08-25 16:52:19 -0400

    did negative testing to assure test fails

--- my-app/src/js/sum.test.js
+++ my-app/src/js/sum.test.js
@@ -2,3 +2,11 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 2 + 3 does not equal 6', () => {
+    expect(sum(2, 3)).not.toBe(6);
+});
+
+test.skip('adds 2 + 3 does equal 6', () => {
+    expect(sum(2, 3)).toBe(6);
+});
